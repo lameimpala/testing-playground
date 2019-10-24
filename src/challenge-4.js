2599f6f91d360c8f51bebf4d9d524bf0778d3b1e
Reid Wixom <reidnwixom@northwesternmutual.com>
2019-10-24 18:25:32 -0500

    Add comment

--- src/challenge-4.js
+++ src/challenge-4.js
@@ -1,3 +1,14 @@
+
+/*
+    Given a string, create a function to reverse the case.
+    All lower-cased letters should be upper-cased, and vice versa.
+
+    reverseCase('sad') === 'SAD';
+    reverseCase('HAPPY') === 'happy';
+    reverseCase('siZZle') === 'SIzzLE';
+*/
+
+
 export function reverseCase(string) {
     throw new Error('Not Implemented');
 }
