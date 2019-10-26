54415e7954d515fb39396256303f40e0e93e7c54
Reid Wixom <reidnwixom@northwesternmutual.com>
2019-10-26 17:15:41 -0500

    Add Challenge-7

---
+++ src/challenge-7.js
@@ -0,0 +1,16 @@
+/*
+    Write a function that adds up the numbers between 0 and the given number, including the given number.
+
+    Examples:
+        addUp(2) = 3 because 1 + 2
+        addUp(3) = 6 because 1 + 2 + 3
+        addUp(-1) = -1 because -1 
+        addUp(-4) = -10 because -1 + -2 + -3 + -4
+
+    Notes:
+        All inputs are numbers
+*/
+
+export function addUp(number) {
+    throw new Error('Not Implemented');
+}
---
+++ src/challenge-7.unit.test.js
@@ -0,0 +1,103 @@
+import { addUp } from './challenge-7';
+
+describe('addUp', () => {
+    it('adds the numbers 0 up to 6', () => {
+        // arrange 
+        const expectedResult = 21;
+
+        // act
+        const actualResult = addUp(6);
+
+        // assert
+        expect(actualResult).toEqual(expectedResult);
+    });
+
+    it('adds the numbers 0 up to 5', () => {
+        // arrange 
+        const expectedResult = 15;
+
+        // act
+        const actualResult = addUp(5);
+
+        // assert
+        expect(actualResult).toEqual(expectedResult);
+    });
+
+    it('adds the numbers 0 up to 4', () => {
+        // arrange 
+        const expectedResult = 10;
+
+        // act
+        const actualResult = addUp(4);
+
+        // assert
+        expect(actualResult).toEqual(expectedResult);
+    });
+
+    it('adds the numbers 0 up to 3', () => {
+        // arrange 
+        const expectedResult = 6;
+
+        // act
+        const actualResult = addUp(3);
+
+        // assert
+        expect(actualResult).toEqual(expectedResult);
+    });
+
+    it('adds the numbers 0 up to 2', () => {
+        // arrange 
+        const expectedResult = 3;
+
+        // act
+        const actualResult = addUp(2);
+
+        // assert
+        expect(actualResult).toEqual(expectedResult);
+    });
+
+
+    it('adds the numbers 0 down to -1', () => {
+        // arrange 
+        const expectedResult = -1;
+
+        // act
+        const actualResult = addUp(-1);
+
+        // assert
+        expect(actualResult).toEqual(expectedResult);
+    });
+
+    it('adds the numbers 0 down to -4', () => {
+        // arrange 
+        const expectedResult = -10;
+
+        // act
+        const actualResult = addUp(-4);
+
+        // assert
+        expect(actualResult).toEqual(expectedResult);
+    });
+
+    it('adds the numbers 0 down to -2', () => {
+        // arrange 
+        const expectedResult = -3;
+
+        // act
+        const actualResult = addUp(-2);
+
+        // assert
+        expect(actualResult).toEqual(expectedResult);
+    });
+
+    it('returns 0 when called with 0', () => {
+        // arrange 
+        const expectedResult = 0;
+
+        // act
+        const actualResult = addUp(0);
+
+        // assert
+        expect(actualResult).toEqual(expectedResult);
+    });
+});
