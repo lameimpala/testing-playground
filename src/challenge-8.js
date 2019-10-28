b08baecccf0af53f676b08638731adf5b36ceb41
Reid Wixom <reidnwixom@northwesternmutual.com>
2019-10-28 14:19:37 -0500

    Add challenge-8

---
+++ src/challenge-8.js
@@ -0,0 +1,14 @@
+/*
+
+    Challenge:
+        Write a function that calculates the third side of a right triangle
+        using the Pythagorean theorem (a^2 + b^2 = c^2)
+
+    Examples:
+        pythagoreanTheorem(3,4) = 5 because 3^2 + 4^2 = 5^2
+
+*/
+
+export function pythagoreanTheorem(a, b) {
+    throw new Error('Not Implemented');
+}
