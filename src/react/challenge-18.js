eaeb1aef9b8be9db1b2bd84a8c9622d8a112a058
Reid Wixom <reidnwixom@northwesternmutual.com>
2020-06-23 20:23:45 -0500

    Add challenges 18 through 20

---
+++ src/react/challenge-18.js
@@ -0,0 +1,52 @@
+/*************************
+    *** Coding Challenge 18
+
+        Create a component that keeps track of a list of "Todo" items,
+        and allows the user to add todos
+
+        The todos are not passed as a prop, rather they are a piece of state internal to the component
+
+        Each todo item should be wrapped in a <li /> tag,
+        and those individual tags should be wrapped in an <ul /> tag
+
+        The input element and handler has been provided to you, but it is up to you to figure out how to "keep track" of the current value of the input
+
+        Multiple pieces of state may be necessary to complete this challenge.  
+*/
+
+import React from 'react';
+
+export function Todos() {
+    // on the next line, I'm destruring the property I need out of the "event" object the function will be called with
+    function onChangeHandlerForInput( { target: { value } } ) {
+        // this is the current value of what is in the input element
+        console.log(value);
+    }
+
+    function onClickHandlerForButton() {
+        // here we can take the value of what is in the input piece of state, and add it to our todos list
+    }
+
+    return (
+        <div>
+            <ul>
+                {
+                    /* 
+                        Todos go here
+                    */
+                } 
+            </ul>
+            <p>
+                <label htmlFor="new-todo-item">New Todo:</label>
+                <input onChange={onChangeHandlerForInput} id="new-todo-item" type="text" />
+            </p> 
+            <p>
+                <button onClick={onClickHandlerForButton}>
+                    Add Todo
+                </button>
+            </p>
+        </div>
+    );
+
+
+}
